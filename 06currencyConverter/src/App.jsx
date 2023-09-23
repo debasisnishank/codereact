505a96430dbb7edc09b6d16f4eef63a068b844c6
debasisnishank <nishankdebasis4@gmail.com>
2023-09-23 10:59:28 +0530

    convert curreny using custom hooks and api call

---
+++ 06currencyConverter/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-center mt-2 font-mono text-3xl bg-orange-500'>Currency Converter</h1>
+    </>
+  )
+}
+ 
+export default App
